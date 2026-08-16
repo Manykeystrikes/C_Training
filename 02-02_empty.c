24c1041d17ff3ccab8e45a2143ece7162fd1de8f
Manykeystrikes <adbart@live.com.au>
2026-08-17 04:45:34 +1000

    upd

--- 02-02_empty.c
+++ 02-02_empty.c
@@ -1,3 +1,9 @@
+/*************************\
+|*   This code written by*|
+|*   some loser          *|
+|*             LOL       *|
+\*************************/
+/*add futher comments related to the code below*/
 #include <stdio.h>
 
 int main()
